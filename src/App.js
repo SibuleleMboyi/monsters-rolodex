648f800f1a9ce70a55779c10af1d58baa0725ef5
SibuleleMboyi <smboyi2016@gmail.com>
2022-06-30 02:44:42 +0200

    optmizing the code for better performance

--- src/App.js
+++ src/App.js
@@ -29,11 +29,28 @@ class App extends Component {
       );
   }
 
+  // Annonymous functions get created and destroyed after each and every run
+  // for this reason, we extracted the annonymous function from the "onChange" handler to here.
+  // This increases optimization as the same function won't get recreated in every run.
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      // same as
+      // return {searchField : searchField}
+      return { searchField };
+    });
+  };
+
   render() {
     console.log("render");
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    // Destructuring Optimization
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this; // or this.onSearchChange
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -41,16 +58,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            //console.log(event);
-            const searchField = event.target.value.toLowerCase();
-
-            this.setState(() => {
-              // same as
-              // return {searchField : searchField}
-              return { searchField };
-            });
-          }}></input>
+          onChange={onSearchChange}></input>
         {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
