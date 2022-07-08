18a8d0f100db1779c406680150f57644932bf656
SibuleleMboyi <smboyi2016@gmail.com>
2022-07-09 00:28:15 +0200

    created a functional component and tested how re-rendering works

--- src/App.js
+++ src/App.js
@@ -1,67 +1,27 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
-import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+  // the functional component does not re-render if the value of "searchField" is the same as the old one
+  console.log(searchField);
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
-
-  // Annonymous functions get created and destroyed after each and every run
-  // for this reason, we extracted the annonymous function from the "onChange" handler to here.
-  // This increases optimization as the same function won't get recreated in every run.
-  onSearchChange = (event) => {
-    console.log(event);
-    const searchField = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      // same as
-      // return {searchField : searchField}
-      return { searchField };
-    });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
-
-  render() {
-    console.log("render1");
-    // Destructuring Optimization
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this; // or this.onSearchChange
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    });
-    return (
-      <div className="App">
-        <h1 className="app-title"> Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-          className="monsters-search-box"></SearchBox>
-        <CardList monsters={filteredMonsters}></CardList>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title"> Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+        className="monsters-search-box"></SearchBox>
+      {/*<CardList monsters={filteredMonsters}></CardList> */}
+    </div>
+  );
+};
 
 export default App;
