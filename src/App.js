9a1107c98bc74532a7cf088c42ebbd3ce792c998
SibuleleMboyi <smboyi2016@gmail.com>
2022-07-09 21:11:06 +0200

    experiementing the need for a useEffect hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import SearchBox from "./components/search-box/search-box.component";
 
+// functional component
+// re-renders whenever the objects and values of the useState() hook change
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
 
-  // the functional component does not re-render if the value of "searchField" is the same as the old one
   console.log(searchField);
 
+  console.log("render");
+
+  // this part of the function continously gets triggered and called everytime seMonsters()
+  // is getting called.
+  // And for this reason, we will optimize the code by the hook called useEffect()
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
